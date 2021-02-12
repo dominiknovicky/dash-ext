ed130fe5a00dc448731681481e311d172a9dab76
dominik.novicky <dominik.novicky24@gmail.com>
2021-02-12 08:06:41 +0100

    done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,71 @@
 import React from "react";
-import { calcNextBirthday } from "../utils";
+import { FormControl, TextField, Button } from "@material-ui/core";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
+import DateFnsUtils from "@date-io/date-fns";
 
-const Login = ({ userLocalStorage }) => {
+const Login = ({
+  handleSubmit,
+  values,
+  handleChange,
+  dateOfBirth,
+  setdateOfBirth,
+}) => {
   return (
-    <div>
-      <div>{userLocalStorage.name}</div>
-      <div>{calcNextBirthday(userLocalStorage)}</div>
-    </div>
+    <form
+      onSubmit={handleSubmit}
+      style={{
+        flexDirection: "column",
+        display: "flex",
+        maxWidth: "300px",
+        width: "100%",
+        padding: "30px 40px",
+        backgroundColor: "rgba(255,255,255,0.5)",
+        boxShadow: "0 0 10px 0 #bfbfbf",
+        borderRadius: 5,
+      }}>
+      <FormControl>
+        <TextField
+          required
+          margin="normal"
+          id="firstName"
+          label="First Name"
+          variant="filled"
+          value={values.firstName}
+          onChange={handleChange("firstName")}
+          type="text"
+        />
+      </FormControl>
+
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <KeyboardDatePicker
+          required
+          margin="normal"
+          disableFuture
+          inputVariant="filled"
+          id="dateOfBirth"
+          label="Date of Birth"
+          format="dd/MM/yyyy"
+          value={dateOfBirth}
+          onChange={setdateOfBirth}
+          KeyboardButtonProps={{
+            "aria-label": "change date",
+          }}
+        />
+      </MuiPickersUtilsProvider>
+
+      <FormControl margin="normal">
+        <Button
+          disabled={!(values.firstName !== "" && dateOfBirth !== null)}
+          variant="contained"
+          color="primary"
+          type="submit">
+          Submit
+        </Button>
+      </FormControl>
+    </form>
   );
 };
 
--- src/styles/BasicStyles.js
+++ src/styles/BasicStyles.js
@@ -9,4 +9,5 @@ export const AppWrapper = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `;
